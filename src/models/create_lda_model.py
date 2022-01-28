38a3bd358b8189edb21835323dac18c56fe9c93a
smzlbm <d.f.prijs@gmail.com>
2022-01-28 10:22:50 +0100

    WIP: generating lda for complete dataset

--- src/models/create_lda_model.py
+++ src/models/create_lda_model.py
@@ -1,6 +1,6 @@
 import time
-import re
 from pprint import pprint
+import sys
 
 import pandas as pd
 import spacy
@@ -39,23 +39,34 @@ def main():
     # Load the raw dataset
     all_cases = load_dataset(DATA_DIR / 'open_data_uitspraken/interim')
 
+    # Delete the summary column as we don't need it
+    all_cases = all_cases.drop(columns=['summary'])
+    print(f'Size of dataset as df: {round(sys.getsizeof(all_cases) / 1024, 2)} kb')
     # Convert the pandas df to a list of dicts for more efficient processing
     cases_dict_list = all_cases.to_dict('records')
 
+    # Temp; testing with batches
+    # print(len(cases_dict_list))
+    cases_dict_list = cases_dict_list#[:10000]
+    # print(len(cases_dict_list))
+
+    del all_cases
+
     # 1. Remove all |'s that were added during data collection
-    cases_dict_list = remove_pipes(cases_dict_list)
+    remove_pipes(cases_dict_list)
 
     # Create LDA model and save it in reports
-    create_lda_model(cases_dict_list)
+    print(f'starting LDA using {len(cases_dict_list)} cases.')
+    create_lda_model(cases_dict_list, save_path=MODELS_DIR / 'lda_full/lda_model')
 
 
 def remove_pipes(list_of_dicts):
     """The prior-added pipes (|) will be filtered out."""
+    start = time.time()
     for i in range(len(list_of_dicts)):
-        list_of_dicts[i]['summary'] = list_of_dicts[i]['summary'].replace("|", " ")
         list_of_dicts[i]['description'] = list_of_dicts[i]['description'].replace("|", " ")
 
-    return list_of_dicts
+    print(f'Time taken to remove pipes from dataset: {round(time.time() - start, 2)} seconds')
 
 
 def tokenize_text(doc, rm_stop_words=False):
@@ -71,45 +82,79 @@ def tokenize_text(doc, rm_stop_words=False):
     return tokenized_text
 
 
-def create_lda_model(dataset):
+def create_lda_model(dataset, save_path):
     """Applies LDA as described in Blei (2003), Latent dirichlet allocation. Used https://towardsdatascience.com/end
     -to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0 as a guide. We follow
-    Bommasani & Cardie (2020), Intrinsic Evaluation of Summarization Datasets, in only using the case descriptions to
-    train the LDA model."""
+    Bommasani & Cardie (2020), Intrinsic Evaluation of Summarization Datasets, in only using the source texts to
+    train the LDA model, and not the summaries."""
     start = time.time()
 
-    # Loop over the cases and preprocess + tokenize the texts + add them to the corpus
-    corpus = []
-    for i in range(len(dataset)):
-        # Create the spacy documents; these can be used to tokenize and sentencize the text
-        summary_doc = nlp(dataset[i]['summary'])
-        text_doc = nlp(dataset[i]['description'])
+    #
+    # Batch implementation
+    #
+    batch_size = round(len(dataset)/10)
+    print(f'Total cases: {len(dataset)}, batch size: {batch_size}')
 
-        # We add both the texts and the summaries together in 1 corpus
-        corpus.append(tokenize_text(summary_doc, rm_stop_words=True))
-        corpus.append(tokenize_text(text_doc, rm_stop_words=True))
+    # First create the base model, with batch 1. We use a dictionary created using only batch one
+    base_corpus = []
+    for case in dataset:
+        base_corpus.append(tokenize_text(nlp(case['description']), rm_stop_words=True))
 
-    # Create a dictionary of all the words contained in the corpus
-    corpus_dictionary = corpora.Dictionary(corpus)
-    # print(len(corpus_dictionary))
+    corpus_dictionary = corpora.Dictionary(base_corpus)
 
-    # Derive the Term Document Frecuencies for each text
-    corpus_tdf = [corpus_dictionary.doc2bow(text) for text in corpus]
+    for obj in locals():
+        print(f'{obj} has size: {round(sys.getsizeof(obj) / 1024, 2)} kb')
 
-    # Learn the LDA model
-    num_topics = 5
-    lda_model = gensim.models.LdaMulticore(corpus=corpus_tdf,
-                                           id2word=corpus_dictionary,
-                                           num_topics=num_topics)
+   # base_tfidf = [corpus_dictionary.doc2bow(text) for text in base_corpus]
+   # print(base_corpus[:2])
+   # num_topics = 5
+   # lda_model = gensim.models.LdaModel(corpus=base_corpus,
+     #                                  id2word=corpus_dictionary,
+    #                                   num_topics=num_topics)
 
+    #
+    #
+    #
+
+    # # Preprocess + tokenize the texts + add them to the corpus, using a generator expression
+    # start_prep = time.time()
+    # corpus = [tokenize_text(nlp(case['description']), rm_stop_words=True) for case in dataset]
+    #
+    # print(f'Total time taken to preprocess the cases '
+    #       f'and combining them into a corpus: {round(time.time() - start_prep, 2)} seconds')
+    # del dataset
+    #
+    # # Create a dictionary of all the words contained in the corpus
+    # corpus_dictionary = corpora.Dictionary(corpus)
+    #
+    # # Derive the Term Document Frecuencies for each text (again using generator expression)
+    # start_tfidf = time.time()
+    # corpus_tdf = [corpus_dictionary.doc2bow(text) for text in corpus]
+    # print(f'Total time taken to compute tf-idf for all cases: {round(time.time() - start_tfidf, 2)} seconds')
+    # del corpus
+    #
+    # # Learn the LDA model, we use 10 batches
+    # # Create the base model with the first 10% of the cases
+    # #corrrir = corpus_tdf
+    # #total_cases = sum(1 for x in corrrir)
+    # #print(f'total cases: {total_cases}, len of base batch: {round(total_cases/10)}')
+    # #base_split =
+    # #for i in range(len(10-1)):
+    #
+    # num_topics = 5
+    # print('creating LDA model')
+    # lda_model = gensim.models.LdaModel(corpus=corpus_tdf,
+    #                                    id2word=corpus_dictionary,
+    #                                    num_topics=num_topics)
+    #
     # Print the Keyword in the 10 topics
-    # pprint(lda_model.print_topics())
+    #pprint(lda_model.print_topics())
+    #
+    # # Save model, and the used dictionary (!), to disk. Note; gensim does not recognize path objects
+    #lda_model.save(str(save_path))
+    # corpus_dictionary.save_as_text(str(save_path))
 
-    # Save model, and the used dictionary (!), to disk.
-    lda_model.save(str(MODELS_DIR / 'lda_model'))
-    corpus_dictionary.save_as_text(str(MODELS_DIR / 'lda_dictionary'))
-
-    print(f'Time taken to perform LDA on dataset: {round(time.time() - start, 2)} seconds')
+    print(f'Total time taken to perform LDA on dataset: {round(time.time() - start, 2)} seconds')
 
 
 if __name__ == '__main__':
